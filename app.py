beb59a7b0121a11f013ad4b24d128be3b16da2f7
kuzuyayuudai <udai928@gmail.com>
2017-07-08 17:27:59 +0900

    database connection easily

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 from flask import render_template
+import MySQLdb
+import config
 
 app = Flask(__name__)
 
@@ -9,6 +11,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', message="Hello World! Hello Flask!")
 
+
+@app.route('/select')
+def db_save():
+    connection = MySQLdb.connect(
+        host=config.HOST, user=config.USER, port=config.PORT, passwd=config.PASSWORD, db=config.DB, charset='utf8')
+    cursor = connection.cursor()
+    sql = u"select * from creative limit 5;"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return render_template('index.html', message=result)
+
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0")
     app.debug()
+
---
+++ config.py
@@ -0,0 +1,7 @@
+# -*-coding:utf-8-*-
+
+DB = "ad_colle_db"
+HOST = "0.0.0.0"
+PORT = 33333
+USER = "root"
+PASSWORD = "root"
